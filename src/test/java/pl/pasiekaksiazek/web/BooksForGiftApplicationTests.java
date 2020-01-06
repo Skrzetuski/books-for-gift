a857d77e700769627b15689ae09eeb724ba08a6e
Krystian Guminski <krystian.guminsk@gmail.com>
2020-01-06 15:47:46 +0100

    Configuration env

---
+++ src/test/java/pl/pasiekaksiazek/web/BooksForGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pasiekaksiazek.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksForGiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
