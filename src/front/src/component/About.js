aa053712308a386ccf4876730b7a9b0febaeca39
Krystian Guminski <krystian.guminsk@gmail.com>
2020-01-17 21:09:07 +0100

    bugfix

--- src/front/src/component/About.js
+++ src/front/src/component/About.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 function About(props) {
 
-    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae ligula eu odio finibus maximus. Nam a eros urna. Ut in mi a arcu facilisis egestas vitae mattis nisi. Fusce sed risus eu turpis dictum sollicitudin vel a erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin a justo orci. Aenean faucibus libero est, ut elementum enim fringilla a. Donec iaculis, est quis tincidunt finibus, orci sem iaculis dolor, ac feugiat urna dui quis libero. Maecenas sollicitudin enim et gravida viverra. Cras maximus dictum est, quis posuere libero egestas nec. Sed eget nulla diam. Quisque quam odio, interdum eu aliquam eget, sodales tincidunt ante. Morbi quis neque enim. Proin placerat lacus viverra sapien scelerisque iaculis. Suspendisse consectetur odio eget tristique commodo. In venenatis enim feugiat, consectetur neque sed, finibus nunc. Duis lorem nisl, rutrum nec finibus in, tristique sed elit. Vivamus bibendum nisl felis, id mollis enim vulputate condimentum. Mauris suscipit lobortis elit. Sed elementum erat sed cursus semper. Fusce ex leo, semper eu malesuada at, porttitor vel ex. Suspendisse gravida tincidunt sem, sed congue diam porttitor nec.";
+
+    const header = <h1>Kim jest Ula?</h1>;
+
+    const p1 = <p className="mt-3">Ula jest dziewczyną, która nie przyjmuje prezentów w formie gotówki.
+                       Natomiast zawsze chętnie znajdzie miejsce na kolejną papierową książkę do
+                       swojej stale rosnącej biblioteczki ;> Ale! Pamiętaj żeby zamieścić dedykację - inaczej
+                       dostaniesz zwrot ;3</p>;
+
+    const p2 = <p>Projekt powstał na moje wewnętrzne potrzeby ułatwienia sobie życia ;3</p>;
+
+    const headerTech = <h1>Technologie</h1>;
+
+    const p1Tech = <p>Projekt oparty jest o takie technologie jak React, Spring Boot oraz AWS.</p>;
 
     if(props.state === true) {
-        return (<section id="about" className="text-justify">{text}</section>);
+        return (<section id="about" className="text-justify" >
+                    {header}
+                    {p1}
+                    {p2}
+
+                    {headerTech}
+                    {p1Tech}
+                </section>);
     } else {
         return ("");
     }
--- src/front/src/component/Contact.js
+++ src/front/src/component/Contact.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 function Contact(props) {
 
+    const email = <p>kontakt@pasiekaksiazek.pl</p>;
+
+    const or = <p>lub</p>;
+
+    const linkedin =<p><a href={"https://www.linkedin.com/in/krystian-gumi%C5%84ski-b77665147/"} target={"_blank"}>Profil Linkedin</a></p>;
+
     if(props.state === true){
-        return (<section id="contact" >Kontakt</section>);
+        return (<section id="contact" >
+            {email}
+            {or}
+            {linkedin}
+        </section>);
     } else {
         return ("");
     }   
